9ffbe704b743f000091d100a952dab86d2fff88c
akmatovbaktybek <byakmatov@gmail.com>
2022-12-12 11:28:59 +0600

    Update home_1.dart

--- home_1/bin/home_1.dart
+++ home_1/bin/home_1.dart
@@ -13,35 +13,55 @@ void main() {
 
   // 2 задание
 
-  
+  int wage = Random().nextInt(500000) + 10000;
+  double fivePercent = wage * 0.05;
+  double sevenPercent = wage * 0.07;
+  double tenPercent = wage * 0.10;
 
-  // 3 задание
+  if (wage < 100000) {
+    print(
+        'Ваша заработная плата равняется $wage не превышает 100 тыс и облагаеся 5% налогом.');
+    print('Сумма налога: $fivePercent');
+    print('Итоговая сумма зарплаты с вычетом налога:${wage - fivePercent}');
+  } else if (wage > 100000 && wage < 200000) {
+    print(
+        'Ваша заработна плата равняется $wage превышает 100 тыc, но  меньше 200 тыс и облагаеся 7% налогом.');
+    print('Сумма налогая: $sevenPercent');
+    print('Итоговая сумма зарплаты с вычетом налога:${wage - sevenPercent}');
+  } else if (wage > 200000) {
+    print(
+        'Ваша заработная плата равняется $wage превышает 200 тыс и облагаеся 10% налогом.');
+    print('Сумма налога: $tenPercent');
+    print('Итоговая сумма зарплаты с вычетом налога:${wage - tenPercent}');
 
-  int seasonYear = Random().nextInt(12) + 1;
+    // 3 задание
 
-  if (seasonYear == 1) {
-    print('Число $seasonYear, месяц - январь, Зима');
-  } else if (seasonYear == 2) {
-    print('Число $seasonYear, месяц - февраль, Зима');
-  } else if (seasonYear == 3) {
-    print('Число $seasonYear, месяц - март, Весна');
-  } else if (seasonYear == 4) {
-    print('Число $seasonYear, месяц - апрель, Весна');
-  } else if (seasonYear == 5) {
-    print('Число $seasonYear, месяц - май, Весна');
-  } else if (seasonYear == 6) {
-    print('Число $seasonYear, месяц - июнь, Лето');
-  } else if (seasonYear == 7) {
-    print('Число $seasonYear, месяц - июль, Лето');
-  } else if (seasonYear == 8) {
-    print('Число $seasonYear, месяц - август, Лето');
-  } else if (seasonYear == 9) {
-    print('Число $seasonYear, месяц - сентябрь, Осень');
-  } else if (seasonYear == 10) {
-    print('Число $seasonYear, месяц - октябрь, Осень');
-  } else if (seasonYear == 11) {
-    print('Число $seasonYear, месяц - ноябрь, Осень');
-  } else if (seasonYear == 12) {
-    print('Число $seasonYear, месяц - Декабрь, Зима');
+    int seasonYear = Random().nextInt(12) + 1;
+
+    if (seasonYear == 1) {
+      print('Число $seasonYear, месяц - январь, Зима');
+    } else if (seasonYear == 2) {
+      print('Число $seasonYear, месяц - февраль, Зима');
+    } else if (seasonYear == 3) {
+      print('Число $seasonYear, месяц - март, Весна');
+    } else if (seasonYear == 4) {
+      print('Число $seasonYear, месяц - апрель, Весна');
+    } else if (seasonYear == 5) {
+      print('Число $seasonYear, месяц - май, Весна');
+    } else if (seasonYear == 6) {
+      print('Число $seasonYear, месяц - июнь, Лето');
+    } else if (seasonYear == 7) {
+      print('Число $seasonYear, месяц - июль, Лето');
+    } else if (seasonYear == 8) {
+      print('Число $seasonYear, месяц - август, Лето');
+    } else if (seasonYear == 9) {
+      print('Число $seasonYear, месяц - сентябрь, Осень');
+    } else if (seasonYear == 10) {
+      print('Число $seasonYear, месяц - октябрь, Осень');
+    } else if (seasonYear == 11) {
+      print('Число $seasonYear, месяц - ноябрь, Осень');
+    } else if (seasonYear == 12) {
+      print('Число $seasonYear, месяц - Декабрь, Зима');
+    }
   }
 }
